d656c759008b6065ba29afaf5b0b508c256d720e
VaheAvetikyan <vahe.avetikyan@gmail.com>
2021-03-23 16:53:20 +0400

    MariaDB config added

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,9 +1,11 @@
 dataSource {
     pooled = true
     jmxExport = true
-    driverClassName = "org.h2.Driver"
-    username = "sa"
-    password = ""
+//    driverClassName = "org.h2.Driver"
+    driverClassName = 'org.mariadb.jdbc.Driver'
+    dialect = "org.hibernate.dialect.MySQL5InnoDBDialect"
+//    username = "sa"
+//    password = ""
 }
 hibernate {
     cache.use_second_level_cache = true
@@ -19,7 +21,11 @@ environments {
     development {
         dataSource {
             dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+//            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+            url = "jdbc:mariadb://localhost:3306/tekdays"
+            // TODO: Change this getenv, may not work with Intellij run configurations
+            username = System.getenv("DB_USER")
+            password = System.getenv("DB_PASSWORD")
         }
     }
     test {
