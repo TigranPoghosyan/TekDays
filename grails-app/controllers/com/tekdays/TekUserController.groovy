e648623e319531b7e13975837b2f7a0794f675e8
TigranPoghosyan <yerevan7971@gmail.com>
2021-03-31 10:41:49 +0400

    Bug fix invalidate action

--- grails-app/controllers/com/tekdays/TekUserController.groovy
+++ grails-app/controllers/com/tekdays/TekUserController.groovy
@@ -13,7 +13,7 @@ class TekUserController {
 
     def index(Integer max) {
         params.max = Math.min(max ?: 10, 100)
-        respond TekUser.list(params), model:[tekUserInstanceCount: TekUser.count()]
+        respond TekUser.list(params), model: [tekUserInstanceCount: TekUser.count()]
     }
 
     def show(TekUser tekUserInstance) {
@@ -32,11 +32,11 @@ class TekUserController {
         }
 
         if (tekUserInstance.hasErrors()) {
-            respond tekUserInstance.errors, view:'create'
+            respond tekUserInstance.errors, view: 'create'
             return
         }
 
-        tekUserInstance.save flush:true
+        tekUserInstance.save flush: true
 
         request.withFormat {
             form multipartForm {
@@ -59,18 +59,18 @@ class TekUserController {
         }
 
         if (tekUserInstance.hasErrors()) {
-            respond tekUserInstance.errors, view:'edit'
+            respond tekUserInstance.errors, view: 'edit'
             return
         }
 
-        tekUserInstance.save flush:true
+        tekUserInstance.save flush: true
 
         request.withFormat {
             form multipartForm {
                 flash.message = message(code: 'default.updated.message', args: [message(code: 'TekUser.label', default: 'TekUser'), tekUserInstance.id])
                 redirect tekUserInstance
             }
-            '*'{ respond tekUserInstance, [status: OK] }
+            '*' { respond tekUserInstance, [status: OK] }
         }
     }
 
@@ -82,14 +82,14 @@ class TekUserController {
             return
         }
 
-        tekUserInstance.delete flush:true
+        tekUserInstance.delete flush: true
 
         request.withFormat {
             form multipartForm {
                 flash.message = message(code: 'default.deleted.message', args: [message(code: 'TekUser.label', default: 'TekUser'), tekUserInstance.id])
-                redirect action:"index", method:"GET"
+                redirect action: "index", method: "GET"
             }
-            '*'{ render status: NO_CONTENT }
+            '*' { render status: NO_CONTENT }
         }
     }
 
@@ -99,24 +99,28 @@ class TekUserController {
                 flash.message = message(code: 'default.not.found.message', args: [message(code: 'tekUser.label', default: 'TekUser'), params.id])
                 redirect action: "index", method: "GET"
             }
-            '*'{ render status: NOT_FOUND }
+            '*' { render status: NOT_FOUND }
         }
     }
+
     def validate() {
         def user = TekUser.findByUserName(params.username)
-        if (user && user.password == params.password){
+        if (user && user.password == params.password) {
             session.user = user
-            redirect controller:'tekEvent', action:'index'
-            if (params.cName)
-                redirect controller: params.cName,action: params.aName
-        }else{
+            if (params.cName) {
+                redirect controller: params.cName, action: params.aName
+            } else {
+                redirect controller: 'tekEvent', action: 'index'
+            }
+        } else {
             flash.message = "Invalid username and password."
-            render view:'login'
+            render view: 'login'
         }
     }
-    def login(){
+
+    def login() {
         if (params.cName)
-            return [cName:params.cName,aName:params.aName]
+            return [cName: params.cName, aName: params.aName]
     }
 
     def logout = {
